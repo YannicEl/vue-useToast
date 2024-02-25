476712bf0aa924fbc0c51e51f8e26299d74ae62a
Yannic <me@yannic.at>
2024-02-25 19:41:44 +0100

    feat: updated default toast

---
+++ packages/lib/src/components/SuccessIcon.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+			<path fill="white" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z" />
+		</svg>
+	</div>
+</template>
+
+<style scoped>
+div {
+	background: #5ab54b;
+}
+</style>
--- packages/lib/src/components/Toast.vue
+++ packages/lib/src/components/Toast.vue
@@ -1,26 +1,18 @@
 <template>
 	<div class="v-toast">
-		<!-- <div class="v-toast-icon">
-			<div
-				v-if="type === 'success'"
-				class="grid h-5 w-5 place-items-center rounded-full bg-green-500"
-			>
-				<div class="i-mdi-check-bold text-white"></div>
-			</div>
+		<SuccessIcon v-if="type === 'success'" class="v-icon" />
+		<ErrorIcon v-if="type === 'error'" class="v-icon" />
+		<Spinner v-if="type === 'loading'" class="v-spinner" />
 
-			<div v-if="type === 'error'" class="grid h-5 w-5 place-items-center rounded-full bg-red-500">
-				<div class="i-mdi-close-bold text-white"></div>
-			</div>
-
-			 <Spinner v-if="type === 'loading'" class="h-5 w-5 stroke-gray-900" />  
-		</div> -->
-
-		<span class="v-toast-message">{{ message }}</span>
+		<span class="toast-message">{{ message }}</span>
 	</div>
 </template>
 
 <script setup lang="ts">
 import type { Toast } from '../composables/useToast';
+import ErrorIcon from './ErrorIcon.vue';
+import Spinner from './Spinner.vue';
+import SuccessIcon from './SuccessIcon.vue';
 
 const { message } = defineProps<Toast>();
 </script>
@@ -28,14 +20,33 @@ const { message } = defineProps<Toast>();
 <style scoped>
 .v-toast {
 	pointer-events: auto;
-	margin-left: auto;
-	margin-right: auto;
 	display: flex;
 	max-width: max-content;
 	align-items: center;
 	gap: 1rem;
 	border-radius: 1rem;
-	background: red;
+	background: white;
 	padding: 1rem;
+	border-radius: 0.375rem;
+	box-shadow:
+		rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+		rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+		rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+		rgba(0, 0, 0, 0.1) 0px 8px 10px -6px;
+}
+
+.v-spinner {
+	width: 1.25rem;
+	height: 1.25rem;
+}
+
+.v-icon {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 1.25rem;
+	height: 1.25rem;
+	border-radius: 100%;
+	padding: 0.125rem;
 }
 </style>
